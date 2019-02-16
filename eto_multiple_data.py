26baa98b590337f8c5f44754008df46bc56b1b97
jaelin <dncgr8@gmail.com>
2019-02-17 01:19:50 +0900

    Create eto_multiple_data.py
    ichiyasa_python 中　干支計算アプリ

---
+++ eto_multiple_data.py
@@ -0,0 +1,8 @@
+# 十二干支求める
+year_str = input('あなたの生まれた年を西暦４桁で入力してください: ')
+year = int(year_str)
+number_of_eto = (year + 8) % 12
+eto_tuple = ('子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申',
+'酉', '戌', '亥')
+eto_name = eto_tuple[number_of_eto]
+print('あなたの干支は', eto_name, 'です。')
