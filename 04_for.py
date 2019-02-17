7847c790efa185c460c8ff85d678f2bf740064cc
jaelin <dncgr8@gmail.com>
2019-02-18 00:13:38 +0900

    range/enumerate

--- 04_for.py
+++ 04_for.py
@@ -8,3 +8,14 @@ number_of_subject = len(point_list)
 average = total/number_of_subject
 
 print('合計点は{},平均点は{}です。'.format(total, average))
+# range
+for number in range(10):
+    print(number)
+
+for number in range(1, 100):
+    print(number)
+
+#enumerate
+
+for index, color in enumerate(['red', 'blue', 'green']):
+    print("No.{} is {}".format(index, color))
